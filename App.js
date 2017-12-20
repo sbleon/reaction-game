466f43f7114dfd91b23cb4fb6f9987c6fb1a519d
Leon Miller-Out <leon@singlebrook.com>
2017-12-19 20:02:12 -0500

    Tap the circle and see your reaction time.

--- App.js
+++ App.js
@@ -1,19 +1,66 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { TouchableWithoutFeedback, StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      circleVisible: false,
+      timeVisible: false,
+    }
+    this.addCircle = this.addCircle.bind(this);
+    this.elapsedTime = this.elapsedTime.bind(this);
+    this.tapCircle = this.tapCircle.bind(this);
+  }
+
+  addCircle() {
+    const startTime = new Date().getTime();
+    this.setState({ circleVisible: true, startTime: startTime });
+  }
+
+  componentDidMount() {
+   setTimeout(this.addCircle, 1000 + Math.random() * 2000);
+  }
+
+  elapsedTime() {
+    return (this.state.endTime - this.state.startTime) + 'ms';
+  }
+
+  tapCircle() {
+    const endTime = new Date().getTime();
+    this.setState({ circleVisible: false, endTime: endTime, timeVisible: true });
+  }
+
   render() {
+    let children = [];
+    if (this.state.circleVisible) {
+      children.push(
+        <TouchableWithoutFeedback key='circle' onPress={this.tapCircle}>
+          <View style={styles.circle} />
+        </TouchableWithoutFeedback>
+      );
+    }
+    if (this.state.timeVisible) {
+      children.push(
+        <Text key='elapsedTime'>{this.elapsedTime()}</Text>
+      )
+    }
+
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={styles.container} id="container">
+        {children}
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  circle: {
+    backgroundColor: 'red',
+    borderRadius: 50,
+    height: 100,
+    width: 100,
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
